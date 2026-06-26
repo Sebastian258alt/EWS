3575db2698ae07361a04346b625776eed432379c
Sebastian258alt <strupsivy@gmail.com>
2026-06-26 13:46:24 +0300

    Delete js directory

--- js/data/a0/index.js
+++
@@ -1,9 +0,0 @@
-const UNITS_A0 = [
-    ...UNITS_A0_UNIT1,
-    ...UNITS_A0_UNIT2,
-    ...UNITS_A0_UNIT3,
-    ...UNITS_A0_UNIT4,
-    ...UNITS_A0_UNIT5,
-    ...UNITS_A0_UNIT6,
-    ...UNITS_A0_UNIT7
-];
